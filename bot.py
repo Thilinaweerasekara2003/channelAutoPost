4d8d201afd3287d905dfef66cc863b1144976389
ThilinaWeerasekara <84437905+Thilinaweerasekara2003@users.noreply.github.com>
2022-05-28 08:54:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def _(event):
 
 @datgbot.on(events.NewMessage(pattern="/help"))
 async def helpp(event):
-    await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel. (without forwarded tag)!\nIt can be used only in two channels at a time, so kindly deploy your own bot from @Hitechrockets \n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!!\n\nLiked the bot? Drop a ♥ to @Prabha_sha :)")
+    await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel. (without forwarded tag)!\nIt can be used only in two channels at a time, so kindly deploy your own bot from @Hitechrockets \n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!!\n\nLiked the bot? Drop a ♥ to @Thilina2004 :)")
 
 @datgbot.on(events.NewMessage(incoming=True, chats=frm)) 
 async def _(event): 
